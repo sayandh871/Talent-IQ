9c89adab47aac8f851a505626535d21fcd8636b1
sayandh <sayandh871@gmail.com>
2025-11-08 09:56:45 +0530

    db and tools set up

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,11 @@ import express from "express";
 import path from "path";
 
 import { ENV } from "./lib/env.js";
+import { connectDb } from "./lib/db.js";
 
 const app = express();
 
 const __dirname = path.resolve();
-
-app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "api is up and running" });
-});
-
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "this is the books endpoint" });
-});
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
@@ -24,4 +17,15 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+const startServer = async () => {
+  try {
+    await connectDb();
+    app.listen(ENV.PORT, () =>
+      console.log("server listening on port", ENV.PORT)
+    );
+  } catch (error) {
+    console.log("Error connecting the server");
+  }
+};
+
+startServer()
